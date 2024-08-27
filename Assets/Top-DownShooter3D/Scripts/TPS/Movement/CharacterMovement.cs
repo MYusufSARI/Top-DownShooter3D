589597d54bbb706f45fb526b08dd582ae31e17a9
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-27 03:51:52 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Movement/CharacterMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.Movement
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterMovement : MonoBehaviour
+    {
+        [Header(" Elements ")]
+        private CharacterController _characterController;
+
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+    }
+}
