c137678e06483c3d1999ce35080cb3a2a12ef415
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-28 08:11:51 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/TransformFollower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS
+{
+    public class TransformFollower : MonoBehaviour
+    {
+        [Header(" Elements ")]
+        [SerializeField] private Transform _target;
+
+
+
+        private void Update()
+        {
+            transform.position = _target.position;
+        }
+    }
+}
