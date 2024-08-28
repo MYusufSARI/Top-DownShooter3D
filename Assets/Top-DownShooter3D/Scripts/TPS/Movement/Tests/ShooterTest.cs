80e645bc2b8a1ae5c195ea90c139cb94a64ee646
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-28 04:35:04 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Movement/Tests/ShooterTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.Movement.Test
+{
+    public class ShooterTest : MonoBehaviour
+    {
+        [Header(" Elements ")]
+        [SerializeField] private GameObject _projectilePrefab;
+
+
+
+        private void Start()
+        {
+            StartCoroutine(Shooter());
+        }
+
+
+        private IEnumerator Shooter()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(0.2f);
+
+                Instantiate(_projectilePrefab, transform.position, transform.rotation);
+            }
+        }
+    }
+}
