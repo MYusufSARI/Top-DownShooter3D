b76043051a5af3686e32a62c2a0725a2571c015b
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-28 03:38:41 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Movement/ProjectileMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.Movement
+{
+    public class ProjectileMovement : MonoBehaviour
+    {
+        [Header(" Settings ")]
+        [SerializeField] private float _speed;
+
+        public float Speed
+        {
+            get => _speed;
+            set => _speed = value;
+        }
+
+
+
+        private void Update()
+        {
+            var direction = transform.forward;
+            var distance = _speed * Time.deltaTime;
+            var targetPosition = transform.position + direction * distance;
+
+            if (Physics.Raycast(transform.position, direction,out var hit, distance))
+            {
+                targetPosition = hit.point;
+            }
+
+            Debug.DrawLine(transform.position, targetPosition, Color.red);
+
+            transform.position = targetPosition;
+
+            Debug.DrawRay(transform.position, direction * distance, Color.blue);
+        }
+    }
+}
