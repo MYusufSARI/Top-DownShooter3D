349f03c690edac6d61f4d379279c8cdee93049be
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-29 16:38:36 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Editor/ShooterEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TPS.Editor
+{
+    [CustomEditor(typeof(Shooter))]
+    public class ShooterEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
