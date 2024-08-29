b1c7e38d1e138ecb8dea8e2f844dbb1824623733
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-29 05:24:55 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/ExplosiveBarrel.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/ExplosiveBarrel.cs
@@ -8,24 +8,61 @@ namespace TPS
     {
         [Header(" Settings ")]
         [SerializeField] private float _health = 5;
-        [SerializeField] private float _explosionRadius;
+        [SerializeField] private float _explosionRadius = 5f;
+        [SerializeField] private float _explosionDamage = 5f;
+        [SerializeField] private float _explosionForce = 50f;
+        [SerializeField] private float _delayBeforeExplosion = 1f;
+        [SerializeField] private AnimationCurve _explosionFalloff;
+
+        private bool _isDead;
+
 
 
 
         public void ApplyDamage(float damage, GameObject cause = null)
         {
+            if (_isDead) return;
+
             _health -= damage;
 
             if (_health <= 0f)
             {
-                Explode();
+                StartCoroutine(ExplodeDelayed());
+                _isDead = true;
             }
+        }
+
+
+        private IEnumerator ExplodeDelayed()
+        {
+            yield return new WaitForSeconds(_delayBeforeExplosion);
+
+            Explode();
         }
 
 
         private void Explode()
         {
             var hits = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+            foreach (var hit in hits)
+            {
+                if (hit.transform == transform) continue;
+
+                var distance = Vector3.Distance(transform.position, hit.transform.position);
+                var rate = distance / _explosionRadius;
+                var falloff = _explosionFalloff.Evaluate(rate);
+
+                if (hit.transform.TryGetComponent<IDamageable>(out var damageable))
+                {
+                    damageable.ApplyDamage(_explosionDamage * falloff);
+                }
+
+                if (hit.attachedRigidbody)
+                {
+                    hit.attachedRigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, 1, ForceMode.Impulse);
+                }
+            }
 
             Destroy(gameObject);
         }
