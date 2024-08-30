b1024848e17d65e0d9a8132b499c60d1d779d2c9
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-30 04:00:30 +0300

    Update ShooterEditor.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/Editor/ShooterEditor.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/Editor/ShooterEditor.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using TPS.WeaponSystem;
 
 namespace TPS.Editor
 {
     [CustomEditor(typeof(Shooter))]
     public class ShooterEditor : UnityEditor.Editor
     {
+        [Header(" Data ")]
+        private Weapon _weapon;
+
+
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+
+            _weapon = EditorGUILayout.ObjectField(_weapon, typeof(Weapon)) as Weapon;
+
+
+
+            if(GUILayout.Button("Switch Weapon"))
+            {
+                var shooter = target as Shooter;
+
+                shooter.EquipWeapon(_weapon);
+            }
         }
     }
 }
