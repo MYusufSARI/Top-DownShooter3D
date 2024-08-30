e19904191d25800750af300250c82ef9d48d07d8
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-30 04:33:00 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/WeaponSystem/WeaponCollectible.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.WeaponSystem
+{
+    public class WeaponCollectible : MonoBehaviour
+    {
+        [Header(" Data ")]
+        [SerializeField] private Weapon _weapon;
+
+
+
+        private void Start()
+        {
+            var inst = Instantiate(_weapon.WeaponGraphics, transform);
+
+            inst.transform.localPosition = Vector3.zero;
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Shooter>(out var shooter))
+            {
+                shooter.EquipWeapon(_weapon);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
