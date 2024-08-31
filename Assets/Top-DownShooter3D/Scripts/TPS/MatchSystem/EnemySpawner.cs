e9356263ee50141dc7d55407570f08267cbaefe5
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-31 08:16:08 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace TPS.MatchSystem
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        
+    }
+}
