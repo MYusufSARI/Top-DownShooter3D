9aea20c7e8bceff86a3a3bae277af5712f8911da
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-08-31 09:51:01 +0300

    Update EnemySpawner.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawner.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,34 @@ namespace TPS.MatchSystem
 {
     public class EnemySpawner : MonoBehaviour
     {
-        
+        [Header(" Elements ")]
+        private Camera _mainCamera;
+
+
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+
+
+
+        private void Start()
+        {
+            StartCoroutine(CreateEnemy());    
+        }
+
+
+        private IEnumerator CreateEnemy()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1);
+
+                var viewportPoint = new Vector3(-0.1f, 0, Mathf.Abs(_mainCamera.transform.position.z));
+
+                var worldPosition = _mainCamera.ViewportToWorldPoint(viewportPoint);
+            }
+        }
     }
 }
