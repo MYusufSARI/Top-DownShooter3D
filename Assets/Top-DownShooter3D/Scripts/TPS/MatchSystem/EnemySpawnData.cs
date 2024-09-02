562b5a5c8e5c892d953adf5e36db9157e60346ce
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-02 07:40:40 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawnData.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawnData.cs
@@ -7,6 +7,19 @@ namespace TPS.MatchSystem
     [CreateAssetMenu(menuName = "EnemySpawnData")]
     public class EnemySpawnData : ScriptableObject
     {
+        [Header("Data")]
+        [SerializeField] private SpawnEntry[] _entries;
+    }
 
+
+    [System.Serializable]
+    public struct SpawnEntry
+    {
+        [Header("Settings")]
+        [SerializeField] private int duration;
+        [SerializeField] private int _spawnCount;
+
+        [Header("Elements")]
+        [SerializeField] private GameObject[] _prefabs;
     }
 }
