45a0d8fcd53374f59094e6e23844f807bacd7c46
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-02 07:20:27 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/EnemySpawnData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.MatchSystem
+{
+    [CreateAssetMenu(menuName = "EnemySpawnData")]
+    public class EnemySpawnData : ScriptableObject
+    {
+
+    }
+}
