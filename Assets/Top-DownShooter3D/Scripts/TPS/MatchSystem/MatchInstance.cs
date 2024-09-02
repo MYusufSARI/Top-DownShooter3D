2dbf6cae1526ff82a2e84a930386963bb6031316
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-02 06:27:18 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/MatchSystem/MatchInstance.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.MatchSystem
+{
+    [CreateAssetMenu(menuName = "MatchInstance")]
+    public class MatchInstance : MonoBehaviour
+    {
+        public float Time { get; set; }
+    }
+}
