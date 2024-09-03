1f9dc758d78637cfa026680b637454ac727df146
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-03 07:44:40 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/Tests/AIMovementTest.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/Tests/AIMovementTest.cs
@@ -1,11 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TPS.MatchSystem;
+using TPS.Movement;
 using UnityEngine;
 
 namespace TPS.Tests
 {
     public class AIMovementTest : MonoBehaviour
     {
+        [Header("Data")]
+        [SerializeField] private MatchInstance _matchInstance;
 
+        private CharacterMovement _characterMovement;
+
+        [Header("Settings")]
+        [SerializeField] private float _acceptanceRadius;
+
+
+
+        private void Awake()
+        {
+            _characterMovement = GetComponent<CharacterMovement>();
+        }
+
+
+        private void Update()
+        {
+            var distance = Vector3.Distance(transform.position, _matchInstance.Player.transform.position);
+
+            if (distance > _acceptanceRadius)
+            {
+                var direction = (_matchInstance.Player.transform.position - transform.position).normalized;
+
+                _characterMovement.MovementInput = new Vector2(direction.x, direction.z);
+            }
+        }
     }
 }
