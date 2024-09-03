6db1364525af361616e59f1feef4a01d484a3e47
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-03 08:15:48 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Mediators/EnemyMediator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.Mediatiors
+{
+    public class EnemyMediator : MonoBehaviour, IDamageable
+    {
+        [Header("Settings")]
+        [SerializeField] private float _health;
+
+
+        public void ApplyDamage(float damage, GameObject causer = null)
+        {
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
