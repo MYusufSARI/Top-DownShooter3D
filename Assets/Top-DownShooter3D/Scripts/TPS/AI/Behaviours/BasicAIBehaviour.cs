64f1a6b89aee9a51f7bd1b8b85bc9cfbd414e176
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-04 07:26:52 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/AI/Behaviours/BasicAIBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.AI.Behaviours
+{
+    public class BasicAIBehaviour : AIBehaviour
+    {
+        public override void Begin(AIController aIController)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void End(AIController aIController)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(AIController aIController)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
