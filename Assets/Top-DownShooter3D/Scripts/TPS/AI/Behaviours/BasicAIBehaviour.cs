097aa526aba12868ee5f9fc4fcd64e222bd2e293
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-04 07:32:07 +0300

    Update BasicAIBehaviour.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/AI/Behaviours/BasicAIBehaviour.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/AI/Behaviours/BasicAIBehaviour.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TPS.MatchSystem;
+using TPS.Movement;
 using UnityEngine;
 
 namespace TPS.AI.Behaviours
 {
+    [CreateAssetMenu(menuName = "AI/Basic AI Behaviour")]
     public class BasicAIBehaviour : AIBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] private float _acceptanceRadius;
+
+        [Header("Data")]
+        [SerializeField] private MatchInstance _matchInstance;
+
+
+
         public override void Begin(AIController aIController)
         {
-            throw new System.NotImplementedException();
+
         }
+
 
         public override void End(AIController aIController)
         {
-            throw new System.NotImplementedException();
+            var player = _matchInstance.Player;
+
+            var movement = aIController.GetComponent<CharacterMovement>();
+
+            var dir = (player.transform.position - aIController.transform.position).normalized;
+
+            movement.MovementInput = new Vector2(dir.x, dir.z);
         }
+
 
         public override void Update(AIController aIController)
         {
-            throw new System.NotImplementedException();
+
         }
     }
 }
