ec8ec30c58f99d8c1d1fc99bb7278384c0fc06bf
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-05 07:38:05 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/EnemyAttacker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS
+{
+    public class EnemyAttacker : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private float _damage;
+        [SerializeField] private float _attackRate;
+
+
+        public void Attack(IDamageable target)
+        {
+
+        }
+    }
+}
