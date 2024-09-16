dd931ad5bba9137d382bc9030d436baa510982ce
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 11:45:32 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Booster.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Booster.cs
@@ -6,6 +6,15 @@ namespace TPS.BoosterSystem
 {
     public abstract class Booster : ScriptableObject
     {
+        [Header("Settings")]
+        [SerializeField] private string _description;
+        [SerializeField] private string _boosterName;
+
+        public string Description => _description;
+        public string BoosterName => _boosterName;
+
+
+
         public abstract void OnAdded(BoosterContainer container);
     }
 }
---
+++ Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/BoosterList.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.BoosterSystem
+{
+    public class BoosterList : ScriptableObject
+    {
+        [Header("Elements")]
+        private List<Booster> _boosters = new List<Booster>();
+
+        public List<Booster> Booster => _boosters;
+    }
+}
