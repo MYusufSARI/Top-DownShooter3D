e44178b65126dae5ebcb3e0df7f6adb2797b5b47
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 11:31:02 +0300

    Update BoosterContainer.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/BoosterContainer.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/BoosterContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,19 @@ namespace TPS.BoosterSystem
 {
     public class BoosterContainer : MonoBehaviour
     {
+        public event Action<Booster> OnBoosterAdded;
 
+        [Header("Elements")]
+        private List<Booster> _boosters = new List<Booster>();
+
+        
+
+
+        public void AddBooster(Booster booster)
+        {
+            _boosters.Add(booster);
+
+            booster.OnAdded(this);
+        }
     }
 }
