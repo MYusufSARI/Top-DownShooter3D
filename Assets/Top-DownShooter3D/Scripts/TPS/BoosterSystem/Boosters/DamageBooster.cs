e780258bc78d03477be2cee67180edededdc441a
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 12:13:33 +0300

    Update DamageBooster.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Boosters/DamageBooster.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Boosters/DamageBooster.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TPS.Mediatiors;
 using UnityEngine;
 
 namespace TPS.BoosterSystem.Boosters
 {
     public class DamageBooster : Booster
     {
+        [Header("Settings")]
+        [SerializeField] private float _value;
+
+
         public override void OnAdded(BoosterContainer container)
         {
-            
+            if(container.TryGetComponent<PlayerMediator>(out var mediator))
+            {
+                mediator.Attributes.Damage += _value;
+            }
         }
     }
 }
