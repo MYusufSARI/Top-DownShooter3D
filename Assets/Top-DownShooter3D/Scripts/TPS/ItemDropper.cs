22df965c4428e17a1685eac42686c5089cb39ca2
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 06:49:37 +0300

    Update ItemDropper.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/ItemDropper.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/ItemDropper.cs
@@ -8,13 +8,21 @@ namespace TPS
     {
         [Header("Settings")]
         [SerializeField] private float _xp;
+        [SerializeField , Range(0,1)] private float _xpDropChange;
 
         [Header("Data")]
         [SerializeField] private XPCollectable _xpCollectablePrefab;
 
+
+
         public void OnDied()
         {
-            
+            if (_xpCollectablePrefab && Random.value > _xpDropChange)
+            {
+                var inst = Instantiate(_xpCollectablePrefab, transform.position, Quaternion.identity);
+
+                inst.XP = _xp;
+            }
         }
     }
 }
