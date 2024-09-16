9adf5e6784927f939b0bfd68fbc7d4077f135dce
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 13:43:18 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/UI/Popup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.UI
+{
+    public class Popup : MonoBehaviour
+    {
+        public bool IsOpen { get; set; }
+
+
+        public void Open()
+        {
+            if (!IsOpen)
+                return;
+
+            gameObject.SetActive(true);
+
+            OnOpened();
+        }
+
+
+        public void Close()
+        {
+            if (IsOpen)
+                return;
+
+            gameObject.SetActive(false);
+
+            OnClosed();
+        }
+
+        protected virtual void OnOpened()
+        {
+            
+        }
+
+
+        protected virtual void OnClosed()
+        {
+
+        }
+    }
+}
