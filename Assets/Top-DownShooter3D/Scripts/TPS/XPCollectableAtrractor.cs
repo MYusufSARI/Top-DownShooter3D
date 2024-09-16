34a667c48308c60e75c81ecd416a5776a5ed135e
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 07:15:16 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/XPCollectableAtrractor.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/XPCollectableAtrractor.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class XPCollectableAtrractor : MonoBehaviour
+namespace TPS
 {
-    // Start is called before the first frame update
-    void Start()
+    public class XPCollectableAtrractor : MonoBehaviour
     {
-        
-    }
+        [Header("Settings")]
+        [SerializeField] private float _tickInterval = 0.7f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+        private void Start()
+        {
+            StartCoroutine(Execute());
+        }
+
+
+        private IEnumerator Execute()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(_tickInterval);
+            }
+        }
     }
 }
