06b4478d300245a9f1c933fb8bd0ad691bf7cb01
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-16 13:12:57 +0300

    .

--- Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Booster.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/BoosterSystem/Booster.cs
@@ -7,11 +7,11 @@ namespace TPS.BoosterSystem
     public abstract class Booster : ScriptableObject
     {
         [Header("Settings")]
-        [SerializeField] private string _description;
         [SerializeField] private string _boosterName;
+        [SerializeField] private string _description;
 
-        public string Description => _description;
         public string BoosterName => _boosterName;
+        public string Description => _description;
 
 
 
