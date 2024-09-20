7381d9fc785c01d85038fd57df199c8fb1a63716
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-20 11:09:51 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/Audio/EnemySFX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.Audio
+{
+    public class EnemySFX : MonoBehaviour
+    {
+        [Header("Data")]
+        [SerializeField] private SoundCue _damageCue;
+        [SerializeField] private SoundCue _attackCue;
+
+        [Header("Elements")]
+        [SerializeField] private AudioSource _audioSource;
+
+
+
+        public void PlayDamagedSFX()
+        {
+            _damageCue.PlayOneShot(_audioSource);
+        }
+
+
+        public void PlayAttackSFX()
+        {
+            _attackCue.PlayOneShot(_audioSource);
+        }
+    }
+}
