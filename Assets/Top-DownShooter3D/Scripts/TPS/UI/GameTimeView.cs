07fb637bf2acae81c38f3a31c6043a626a311cd3
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-21 10:12:39 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/UI/GameTimeView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TPS.MatchSystem;
+using UnityEngine;
+
+namespace TPS.UI
+{
+    public class GameTimeView : MonoBehaviour
+    {
+        [Header("Data")]
+        [SerializeField] private MatchInstance _matchInstance;
+    }
+}
