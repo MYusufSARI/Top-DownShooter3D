ea1ccf9825256a6c056df3dab87a5b7484efa197
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-21 10:19:49 +0300

    Update GameTimeView.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/UI/GameTimeView.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/UI/GameTimeView.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using TPS.MatchSystem;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TPS.UI
 {
@@ -9,5 +12,17 @@ namespace TPS.UI
     {
         [Header("Data")]
         [SerializeField] private MatchInstance _matchInstance;
+
+        [Header("Elements")]
+        [SerializeField] private TMP_Text _text;
+
+
+
+        private void Update()
+        {
+            var timeSpan = TimeSpan.FromSeconds(_matchInstance.Time);
+
+            _text.text = string.Format("{0:02}:{1:02}", timeSpan.Minutes, timeSpan.Seconds); 
+        }
     }
 }
