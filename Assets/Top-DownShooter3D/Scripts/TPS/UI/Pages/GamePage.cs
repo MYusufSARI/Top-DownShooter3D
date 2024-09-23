429e33d8d78e092310ec26f846ccdec56018a34d
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-23 19:27:41 +0300

    Update GamePage.cs

--- Assets/Top-DownShooter3D/Scripts/TPS/UI/Pages/GamePage.cs
+++ Assets/Top-DownShooter3D/Scripts/TPS/UI/Pages/GamePage.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TPS.UI.Pagination;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace TPS.UI.Pages
 {
     public class GamePage : Page
     {
+        [Header("Elements")]
+        [SerializeField] private Button _playButton;
+
+
+
         protected override void OnOpened()
         {
-            
+            _playButton.onClick.AddListener(OnPlayButtonPressed);
         }
     
 
         protected override void OnClosed()
         {
-            
+            _playButton.onClick.RemoveListener(OnPlayButtonPressed);
+        }
+
+
+        private void OnPlayButtonPressed()
+        {
+            SceneManager.LoadScene(1);
         }
     }
 }
