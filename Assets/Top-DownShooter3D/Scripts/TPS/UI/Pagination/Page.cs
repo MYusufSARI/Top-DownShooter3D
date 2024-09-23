bab48eb22a0054273882f0f3bf75c08ef8ebe721
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-09-23 14:49:12 +0300

    .

---
+++ Assets/Top-DownShooter3D/Scripts/TPS/UI/Pagination/Page.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPS.UI.Pagination
+{
+    public class Page : MonoBehaviour
+    {
+        public void Open()
+        {
+
+        }
+
+
+        public void Close()
+        {
+
+        }
+
+        protected virtual OnOpened()
+        {
+
+        }
+
+
+        protected virtual Onclosed()
+        {
+
+        }
+    }
+}
